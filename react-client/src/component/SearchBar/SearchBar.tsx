15525310ea7958c794ac7eb156a31525f8191781
fa <zuchairul@gmail.com>
2022-12-28 15:28:23 +0800

    fix icon

--- react-client/src/component/SearchBar/SearchBar.tsx
+++ react-client/src/component/SearchBar/SearchBar.tsx
@@ -1,11 +1,17 @@
 import { BiSearch } from 'react-icons/bi'
 import './style.css'
+import { IconContext } from 'react-icons/lib'
 
 const SearchBar = () => {
   return (
     <div className="SearchBar">
-      <input type="text" placeholder='Search here' />
-      <span><BiSearch /></span>
+      <input type="text" placeholder='Search' />
+      <IconContext.Provider value={{
+        color: "rgb(133, 133, 133)",
+        size: "20.25px"
+      }}>
+        <BiSearch />
+      </IconContext.Provider>
     </div>
   )
 }
