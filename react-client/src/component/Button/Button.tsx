36d8627afd4e6b243a85eacaa5d8da03cbb92912
fa <zuchairul@gmail.com>
2022-12-28 15:54:43 +0800

    fix button

--- react-client/src/component/Button/Button.tsx
+++ react-client/src/component/Button/Button.tsx
@@ -1,8 +1,13 @@
+import { IconContext } from 'react-icons/lib'
 import './style.css'
+import { BsPlusLg } from 'react-icons/bs'
 
 const Button = () => {
   return (
-    <button className='Button'>+ Add Task</button>
+    <button className='Button'>
+      <BsPlusLg />
+      Add Task
+    </button>
   )
 }
 
